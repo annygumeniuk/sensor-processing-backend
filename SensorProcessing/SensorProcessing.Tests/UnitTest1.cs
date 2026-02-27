e9b14f1a675fd088b7a61a9a7f82ebf6c6e861ee
annygumeniuk <anny.gumeniuk@gmail.com>
2026-02-27 16:18:04 +0200

    Added new project for tests

---
+++ SensorProcessing/SensorProcessing.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SensorProcessing.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
